21e8e5ad9b1ae4f727485a275442d79b8fa57e45
Fabian Hingerl <fabian.hingerl@gmail.com>
2024-06-12 18:53:05 +0200

    Reserve memory for candidates instead of allocating a vector of length so that successive shrink_to_fit correctly resizes candidate vector

--- include/optimizations/blockedClauseElimination/candidateSelection/baseCandidateSelector.hpp
+++ include/optimizations/blockedClauseElimination/candidateSelection/baseCandidateSelector.hpp
@@ -18,12 +18,13 @@ namespace blockedClauseElimination {
 		[[nodiscard]] virtual std::vector<std::size_t> determineCandidates()
 		{
 			const std::size_t numCandidatesToChoseFrom = std::min(optionalMaximumNumberOfCandidates.value_or(problemDefinition->getNumClauses()), problemDefinition->getNumClauses());
-			std::vector<std::size_t> chooseableCandidateIndices = std::vector(numCandidatesToChoseFrom, static_cast<std::size_t>(0));
-			std::size_t nextContainerPositionIdx = 0;
+			std::vector<std::size_t> chooseableCandidateIndices;
+			chooseableCandidateIndices.reserve(numCandidatesToChoseFrom);
+
 			for (std::size_t i = 0; i < numCandidatesToChoseFrom; ++i)
 			{
 				if (const std::optional<dimacs::ProblemDefinition::Clause*> matchingClauseForIdx = problemDefinition->getClauseByIndexInFormula(i); matchingClauseForIdx.has_value() && matchingClauseForIdx.value()->literals.size() > 1)
-					chooseableCandidateIndices.at(nextContainerPositionIdx++) = i;
+					chooseableCandidateIndices.emplace_back(i);
 			}
 			chooseableCandidateIndices.shrink_to_fit();
 			return chooseableCandidateIndices;
