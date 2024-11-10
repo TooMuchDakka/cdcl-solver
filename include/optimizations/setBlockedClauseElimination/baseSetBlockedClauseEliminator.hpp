12c5b34e93e8f83bbddbd6f02375c308c12c32f4
Fabian Hingerl <fabian.hingerl@gmail.com>
2024-11-10 22:38:26 +0100

    Moved implementation of check for blocking set to base class and refactored literal occurrence set blocked clause eliminator to header only implementationRemoved unneeded blocking set candidate generator private member

--- include/optimizations/setBlockedClauseElimination/baseSetBlockedClauseEliminator.hpp
+++ include/optimizations/setBlockedClauseElimination/baseSetBlockedClauseEliminator.hpp
@@ -23,7 +23,29 @@ namespace setBlockedClauseElimination {
 		{
 			return determineBlockingSet(clauseIdxInFormula, candidateGenerator, std::nullopt);
 		}
-		[[nodiscard]] virtual std::optional<FoundBlockingSet> determineBlockingSet(std::size_t clauseIdxInFormula, BaseBlockingSetCandidateGenerator& candidateGenerator, const std::optional<BaseBlockingSetCandidateGenerator::CandidateSizeRestriction>& optionalCandidateSizeRestriction) const = 0;
+		[[nodiscard]] virtual std::optional<FoundBlockingSet> determineBlockingSet(std::size_t clauseIdxInFormula, BaseBlockingSetCandidateGenerator& candidateGenerator, const std::optional<BaseBlockingSetCandidateGenerator::CandidateSizeRestriction>& optionalCandidateSizeRestriction)
+		{
+			const dimacs::ProblemDefinition::Clause* dataOfAccessedClause = problemDefinition->getClauseByIndexInFormula(clauseIdxInFormula);
+			if (!dataOfAccessedClause || dataOfAccessedClause->literals.size() < 2)
+				return std::nullopt;
+
+			const std::vector<long>& clauseLiterals = dataOfAccessedClause->literals;
+			candidateGenerator.init(clauseLiterals, problemDefinition->getLiteralOccurrenceLookup(), optionalCandidateSizeRestriction);
+
+			std::optional<BaseBlockingSetCandidateGenerator::BlockingSetCandidate> candidateBlockingSet = candidateGenerator.generateNextCandidate();
+			bool foundBlockingSet = false;
+			while (!foundBlockingSet && candidateBlockingSet.has_value())
+			{
+				const std::unordered_set<long>& clauseLiteralsAndBlockingSetDifferenceSet = determineDifferenceSetBetweenClauseAndBlockingSet(clauseLiterals, *candidateBlockingSet);
+				foundBlockingSet |= doesEveryClauseInResolutionEnvironmentFullfillSetBlockedCondition(clauseLiteralsAndBlockingSetDifferenceSet, *candidateBlockingSet);
+				if (!foundBlockingSet)
+					candidateBlockingSet = candidateGenerator.generateNextCandidate();
+			}
+			if (foundBlockingSet && candidateBlockingSet.has_value())
+				return BaseSetBlockedClauseEliminator::FoundBlockingSet(candidateBlockingSet->cbegin(), candidateBlockingSet->cend());
+
+			return std::nullopt;
+		};
 
 		// TODO: Enable search for blocking set of specific size?
 
@@ -74,6 +96,17 @@ namespace setBlockedClauseElimination {
 
 		[[nodiscard]] static bool canCheckForBlockingSetBePerformed(const dimacs::ProblemDefinition::Clause& clause) noexcept {
 			return clause.literals.size() > 1;
+		}
+
+		[[nodiscard]] static std::unordered_set<long> determineDifferenceSetBetweenClauseAndBlockingSet(const std::vector<long>& clauseLiterals, const BaseBlockingSetCandidateGenerator::BlockingSetCandidate& blockingSet)
+		{
+			std::unordered_set<long> differenceSet;
+			std::set<long, std::less<>> orderedBlockingSet(blockingSet.cbegin(), blockingSet.cend());
+			std::set_difference(
+				clauseLiterals.cbegin(), clauseLiterals.cend(),
+				orderedBlockingSet.cbegin(), orderedBlockingSet.cend(),
+				std::inserter(differenceSet, differenceSet.begin()));
+			return differenceSet;
 		}
 	};
 }
--- include/optimizations/setBlockedClauseElimination/literalOccurrenceSetBlockedClauseEliminator.hpp
+++ include/optimizations/setBlockedClauseElimination/literalOccurrenceSetBlockedClauseEliminator.hpp
@@ -1,8 +1,6 @@
 #ifndef CLAUSE_LITERAL_LOOKUP_SET_BLOCKED_CLAUSE_ELIMINATOR_HPP
 #define CLAUSE_LITERAL_LOOKUP_SET_BLOCKED_CLAUSE_ELIMINATOR_HPP
 
-#include <set>
-
 #include "baseSetBlockedClauseEliminator.hpp"
 #include "literalOccurrenceBlockingSetCandidateGenerator.hpp"
 #include <dimacs/literalOccurrenceLookup.hpp>
@@ -14,20 +12,15 @@ namespace setBlockedClauseElimination {
 
 		LiteralOccurrenceSetBlockedClauseEliminator() = delete;
 		explicit LiteralOccurrenceSetBlockedClauseEliminator(dimacs::ProblemDefinition::ptr problemDefinition)
-			: BaseSetBlockedClauseEliminator(std::move(problemDefinition))
-		{
-			candidateGenerator = std::make_unique<LiteralOccurrenceBlockingSetCandidateGenerator>();
-			if (!candidateGenerator)
-				throw std::exception("Failed to initialize candidate generator");
-		}
+			: BaseSetBlockedClauseEliminator(std::move(problemDefinition)) {}
 
-		[[nodiscard]] std::optional<std::vector<long>> determineBlockingSet(std::size_t clauseIdxInFormula, BaseBlockingSetCandidateGenerator& candidateGenerator, const std::optional<BaseBlockingSetCandidateGenerator::CandidateSizeRestriction>& optionalCandidateSizeRestriction) const override;
-		
 	protected:
-		LiteralOccurrenceBlockingSetCandidateGenerator::ptr candidateGenerator;
-
-		[[nodiscard]] std::unordered_set<std::size_t> determineIndicesOfOverlappingClausesForLiteral(long literal) const override;
-		[[nodiscard]] static std::unordered_set<long> determineDifferenceSetBetweenClauseAndBlockingSet(const std::vector<long>& clauseLiterals, const BaseBlockingSetCandidateGenerator::BlockingSetCandidate& blockingSet);
+		[[nodiscard]] std::unordered_set<std::size_t> determineIndicesOfOverlappingClausesForLiteral(long literal) const override
+		{
+			if (const dimacs::LiteralOccurrenceLookup::LiteralOccurrenceLookupEntry* indicesOfClauesContainingNegatedLiteral = problemDefinition->getLiteralOccurrenceLookup()[-literal].value_or(nullptr); indicesOfClauesContainingNegatedLiteral)
+				return *indicesOfClauesContainingNegatedLiteral;
+			return {};
+		}
 	};
 }
 #endif
